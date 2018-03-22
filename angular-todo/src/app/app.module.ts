3efaca12fc5e7fc3ab8ce1ff2057ea81f0574a53
Askar <aleemov@gmail.com>
2018-03-22 22:37:21 +0600

    Base html modifications were made. Forms and Common Modules added for app.module.ts

--- angular-todo/src/app/app.module.ts
+++ angular-todo/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodoListComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        CommonModule
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
